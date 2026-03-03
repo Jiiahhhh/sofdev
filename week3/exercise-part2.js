31c3df98bef8781cf6bd9cd79e6e0177a291ecb1
ilal <ijlalnurhadi01@gmail.com>
2026-03-04 01:35:08 +0800

    uncomment week 3 part 2

--- week3/exercise-part2.js
+++ week3/exercise-part2.js
@@ -1,50 +1,50 @@
-// // Soal 1
-// fetch("https://jsonplaceholder.typicode.com/users")
-//   .then((response) => response.json())
-//   .then((users) => {
-//     users.forEach((data) => {
-//       console.log(`id: ${data.id}, name: ${data.name}, email: ${data.email}`);
-//     });
-//   });
+// Soal 1
+fetch("https://jsonplaceholder.typicode.com/users")
+  .then((response) => response.json())
+  .then((users) => {
+    users.forEach((data) => {
+      console.log(`id: ${data.id}, name: ${data.name}, email: ${data.email}`);
+    });
+  });
 
-// // Soal 2
-// function getPostsByUser(userId) {
-//   fetch(`https://jsonplaceholder.typicode.com/posts?userId=${userId}`)
-//     .then((response) => {
-//       if (!response.ok) {
-//         throw new Error("User not found!");
-//       }
-//       return response.json();
-//     })
-//     .then((posts) => {
-//       console.log(`Count posts: ${posts.length}`);
-//       posts.forEach((data) => {
-//         console.log(data.title);
-//       });
-//     });
-// }
+// Soal 2
+function getPostsByUser(userId) {
+  fetch(`https://jsonplaceholder.typicode.com/posts?userId=${userId}`)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("User not found!");
+      }
+      return response.json();
+    })
+    .then((posts) => {
+      console.log(`Count posts: ${posts.length}`);
+      posts.forEach((data) => {
+        console.log(data.title);
+      });
+    });
+}
 
-// getPostsByUser(1);
+getPostsByUser(1);
 
-// // Soal 3
-// fetch("https://jsonplaceholder.typicode.com/invalid-endpoint")
-//   .then((response) => {
-//     if (!response.ok) {
-//       throw new Error(`HTTP Error: ${response.status}`);
-//     }
-//   })
-//   .catch((error) => console.log("Error: ", error.message));
+// Soal 3
+fetch("https://jsonplaceholder.typicode.com/invalid-endpoint")
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error(`HTTP Error: ${response.status}`);
+    }
+  })
+  .catch((error) => console.log("Error: ", error.message));
 
-// // Soal 4
-// function displayTodos() {
-//   fetch("https://jsonplaceholder.typicode.com/posts/todos?userId=1").then(
-//     (response) => {
-//       if (!response.ok) {
-//         throw new Error(`HTTP Error: ${response.status}`);
-//       }
-//     },
-//   );
-// }
+// Soal 4
+function displayTodos() {
+  fetch("https://jsonplaceholder.typicode.com/posts/todos?userId=1").then(
+    (response) => {
+      if (!response.ok) {
+        throw new Error(`HTTP Error: ${response.status}`);
+      }
+    },
+  );
+}
 
 // Soal 5
 fetch("https://jsonplaceholder.typicode.com/users/1")
