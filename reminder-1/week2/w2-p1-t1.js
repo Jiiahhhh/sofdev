2e5f9feaab48f202b96c60cb17090c92dcf5e19c
ilal <ijlalnurhadi01@gmail.com>
2026-04-06 08:35:56 +0800

    rename reminder week 1

---
+++ reminder-1/week2/w2-p1-t1.js
@@ -0,0 +1,7 @@
+const scores = [72, 85, 91, 60, 78, 95, 83, 67, 88, 74];
+
+const passed = scores.filter((n) => n >= 75);
+console.log(passed);
+
+const doubled = scores.map((n) => n * 2);
+console.log(doubled);
